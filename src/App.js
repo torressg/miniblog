a7a80ea3db87ee45a112f3927efd47dd382bb67b
Guilherme Torres <guilhermevanderlei0210@gmail.com>
2023-08-12 09:07:38 -0300

    Start the AuthMethod

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import styles from './App.css';
+
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+
 import { Home } from './pages/Home/Home';
 import { About } from './pages/About/About';
 import { Navbar } from './components/Navbar/Navbar';
@@ -7,21 +10,27 @@ import { Footer } from './components/Footer/Footer';
 import { Login } from './pages/Login/Login';
 import { Register } from './pages/Register/Register';
 
+import { AuthProvider } from './context/AuthContext';
+import { onAuthStateChanged } from 'firebase/auth';
+import { useAuthentication } from './hooks/useAuthentication';
+
 function App() {
   return (
     <div className={styles.container}>
-      <BrowserRouter>
-        <Navbar />
-        <div>
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/about' element={<About />} />
-            <Route path='/register' element={<Register />} />
-            <Route path='/login' element={<Login />} />
-          </Routes>
-        </div>
-        <Footer />
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <Navbar />
+          <div>
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/about' element={<About />} />
+              <Route path='/register' element={<Register />} />
+              <Route path='/login' element={<Login />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+      </AuthProvider>
     </div>
   );
 }
