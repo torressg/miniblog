75f17a9985d7ee7ac2cd9890fdfa37bc5c2535c3
Guilherme Torres <guilhermevanderlei0210@gmail.com>
2023-08-14 09:43:54 -0300

    Custom context to user

--- src/App.js
+++ src/App.js
@@ -9,23 +9,43 @@ import { Navbar } from './components/Navbar/Navbar';
 import { Footer } from './components/Footer/Footer';
 import { Login } from './pages/Login/Login';
 import { Register } from './pages/Register/Register';
+import { CreatePost } from './pages/CreatePost/CreatePost';
+import { Dashboard } from './pages/Dashboard/Dashboard';
 
 import { AuthProvider } from './context/AuthContext';
 import { onAuthStateChanged } from 'firebase/auth';
 import { useAuthentication } from './hooks/useAuthentication';
 
+
 function App() {
+
+  const [user, setUser] = useState(undefined)
+  const { auth } = useAuthentication()
+
+  const loadingUser = user === undefined
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setUser(user)
+    })
+  }, [auth])
+  if (loadingUser) {
+    return <p>Carregando...</p>
+  }
+
   return (
     <div className={styles.container}>
-      <AuthProvider>
+      <AuthProvider value={{ user }}>
         <BrowserRouter>
           <Navbar />
           <div>
             <Routes>
               <Route path='/' element={<Home />} />
               <Route path='/about' element={<About />} />
-              <Route path='/register' element={<Register />} />
-              <Route path='/login' element={<Login />} />
+              <Route path='/register' element={!user ? <Register /> : <Navigate to='/' />} />
+              <Route path='/login' element={!user ? <Login /> : <Navigate to='/' />} />
+              <Route path='/posts/create' element={user ? <CreatePost /> : <Navigate to='/login' />} />
+              <Route path='/dashboard' element={user ? <Dashboard/> : <Navigate to='/login' />} />
             </Routes>
           </div>
           <Footer />
---
+++ src/pages/CreatePost/CreatePost.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from './CreatePost.module.css'
+
+export const CreatePost = () => {
+  return (
+    <div>CreatePost</div>
+  )
+}
